404f4b4353b39a19201c63c4849315fbbed9618c
Alejandro Arce <65831986+0ALJD0@users.noreply.github.com>
2021-11-21 21:37:41 -0500

    Add files via upload

---
+++ Proyecto/EscogeUsuario/code.js
@@ -0,0 +1,13 @@
+
+document.querySelector('#consultar').addEventListener('click', 
+VerHorario);
+
+function VerHorario(){
+    window.location.href='../VistaUsuario/VistaUsuario.html' 
+}
+
+document.querySelector('#ver').addEventListener('click', 
+VerPerfil);
+function VerPerfil(){
+    window.location.href='../Perfil/perfil.html' 
+}
